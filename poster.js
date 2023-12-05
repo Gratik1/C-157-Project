9b386340dfb937e3582a2e02b15286aaa5dae76c
Gratik1 <103760215+Gratik1@users.noreply.github.com>
2023-12-05 19:53:05 +0530

    Add files via upload

---
+++ poster.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent("comics-poster", {
+
+    init: function(){
+        this.createPoster()
+    },
+
+    createPoster: function(item) {
+        const entityEl = document.createElement("a-entity");
+        entityEl.setAttribute("visible", true);
+        entityEl.setAttribute("geometry", {
+            primitive: "plane",
+            width: 20,
+            height: 28
+        });
+
+        entityEl.setAttribute("position", {x: 0, y: 5, z: 0.1})
+        entityEl.setAttribute("material", {src: item.url})
+
+    }
+})
